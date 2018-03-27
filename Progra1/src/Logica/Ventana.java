f195f61fe24fc897b7e2134fed7098d7ddab4072
karlami <karla.rivera24@gmail.com>
2018-03-27 14:44:43 -0600

    Ventana hereada de JFrame, e implementa a WindowListener y sus metodos abstractos

--- Progra1/src/Logica/Ventana.java
+++ Progra1/src/Logica/Ventana.java
@@ -1,6 +1,61 @@
 
 package Logica;
+import javax.swing.JFrame;
+import java.awt.event.*;
+public class Ventana extends JFrame implements WindowListener{
+    
+    /**
+     * Constructor de la clase Ventana
+     * Llama al constructor del padre: JFrame y le manda el nombre del titulo
+     * @param titulo que contiene el nombre del titulo del juego
+     */
+    
+    public Ventana(String titulo){
+        super(titulo);
+        setSize(500,500);
+        addWindowListener(this);
+        
+    }
 
-public class Ventana {
+    @Override
+    public void windowOpened(WindowEvent e) {
+        
+    }
+    
+    /**
+     * Nos permite cerrar la ventana
+     * @param e 
+     */
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        System.exit(0);
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+        
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+        
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+        
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+        
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+        
+    }
+
     
 }
