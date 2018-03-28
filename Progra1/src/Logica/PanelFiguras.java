296a59d2b7996ab1e2f3adad99bb538442a58a39
karlami <karla.rivera24@gmail.com>
2018-03-28 14:24:44 -0600

    Darle movimiento a la nave, se vuelve a pintar a la nueva posicion

--- Progra1/src/Logica/PanelFiguras.java
+++ Progra1/src/Logica/PanelFiguras.java
@@ -4,11 +4,17 @@
 package Logica;
 import EstructurasDatos.ListaSimple;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import javax.swing.JPanel;
-public class PanelFiguras extends JPanel{
+public class PanelFiguras extends JPanel implements KeyListener{
     
     ListaSimple lista;
     
+    TrianguloGrafico nave;
+    Coordenada movimientoIzq = new Coordenada(-25,0); //mov x en izquierda
+    Coordenada movimientoDer = new Coordenada(25,0); //mov x en izquierda
+    Coordenada movimientoNulo = new Coordenada(0,0); //mov x nulo
     /**
      * Constructor de la clase PanelFiguras
      * @param v todos los objetos a dibujar en panel que recibe del Main
@@ -17,6 +23,8 @@ public class PanelFiguras extends JPanel{
     public PanelFiguras(ListaSimple v){
         
         this.lista = v;
+        this.addKeyListener(this);
+        setFocusable(true);
         
     }
     /**
@@ -24,6 +32,7 @@ public class PanelFiguras extends JPanel{
      * que recorre la lista de objetos
      * @param g 
      */
+    @Override
     public void paint(Graphics g){
         
         Dibujable dib;
@@ -35,6 +44,47 @@ public class PanelFiguras extends JPanel{
             dib = (Dibujable)lista.get(i);
             dib.dibujar(g);
         }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int tecla = e.getKeyCode();
+        
+        if(tecla == KeyEvent.VK_LEFT){
+            this.nave.pintar(this.getGraphics(), Color.black);
+            this.nave.mover(movimientoIzq);
+            this.nave.pintar(this.getGraphics(), Color.pink);
+        }
+        if(tecla == KeyEvent.VK_RIGHT){
+            this.nave.pintar(this.getGraphics(), Color.black);
+            this.nave.mover(movimientoDer);
+            this.nave.pintar(this.getGraphics(), Color.pink);
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int tecla = e.getKeyCode();
+        
+        if(tecla == KeyEvent.VK_LEFT){
+            
+            this.nave.mover(movimientoNulo);
+            
+        }
+        if(tecla == KeyEvent.VK_RIGHT){
+            
+            this.nave.mover(movimientoNulo);
+            
+        }
+    }
+    
+    public void refNave(TrianguloGrafico n){
+        this.nave = n;
     }
     
 }
