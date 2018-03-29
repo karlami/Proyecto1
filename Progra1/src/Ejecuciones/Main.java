540b2e3f83fc733365df0901441896d0795ccb44
karlami <karla.rivera24@gmail.com>
2018-03-29 00:05:41 -0600

    Se crea un metodo aleatorio que escoge un numero de un rango requerido, se escogen 5 rangos para dar 5 numeros random para dar una nueva coordenada para cada uno de los 5 enemigos que son instanciados e ingresados en la lista de objetos a dibujar

--- Progra1/src/Ejecuciones/Main.java
+++ Progra1/src/Ejecuciones/Main.java
@@ -5,6 +5,10 @@ import EstructurasDatos.*;
 import java.awt.*;
 
 public class Main {
+    
+    public static int aleatorio(int max, int min){
+        return (int)(Math.random() * (max-min));
+    }
 
     /**
      * @param args the command line arguments
@@ -27,13 +31,41 @@ public class Main {
         
         //listaDeObjetos.add(enemigo);
         //listaDeObjetos.add(bala);
+        
+        int rango = aleatorio(800,50);
+        Coordenada salida = new Coordenada(rango,0);
+        RectanguloGrafico enemigo1 = new RectanguloGrafico(salida,25,25,Color.red);
+        
+        int rango2 = aleatorio(800,50);
+        Coordenada salida2 = new Coordenada(rango2,0);
+        RectanguloGrafico enemigo2 = new RectanguloGrafico(salida2,25,25,Color.red);
+        
+        int rango3 = aleatorio(800,50);
+        Coordenada salida3 = new Coordenada(rango3,0);
+        RectanguloGrafico enemigo3 = new RectanguloGrafico(salida3,25,25,Color.red);
+        
+        int rango4 = aleatorio(800,50);
+        Coordenada salida4 = new Coordenada(rango4,0);
+        RectanguloGrafico enemigo4 = new RectanguloGrafico(salida4,25,25,Color.red);
+        
+        int rango5 = aleatorio(800,50);
+        Coordenada salida5 = new Coordenada(rango5,0);
+        RectanguloGrafico enemigo5 = new RectanguloGrafico(salida5,25,25,Color.red);
+        
         listaDeObjetos.add(nave);
+        listaDeObjetos.add(enemigo1);
+        listaDeObjetos.add(enemigo2);
+        listaDeObjetos.add(enemigo3);
+        listaDeObjetos.add(enemigo4);
+        listaDeObjetos.add(enemigo5);
         
         PanelFiguras panel = new PanelFiguras(listaDeObjetos);
         panel.refNave(nave);
+        panel.refEnem(enemigo1, enemigo2, enemigo3, enemigo4, enemigo5);
         ventana.add(panel);
         ventana.setSize(800,600);
         ventana.setVisible(true);
+        panel.iniciar();
         
         ListaSimple li = new ListaSimple();
         li.add(5);
